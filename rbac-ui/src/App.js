725fff759780161a0156852061d556a434a86db8
PrathameshDurgude06 <pdurgude63@gmail.com>
2024-11-24 16:43:49 +0530

    initial setup

---
+++ rbac-ui/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-6 text-center">
+      <h1 className="text-3xl font-bold">Hello, Tailwind!</h1>
+      <p className="mt-2">Tailwind CSS is now working!</p>
+    </div>
+  );
+}
+
+export default App;
